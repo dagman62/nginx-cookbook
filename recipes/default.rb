4acf8c5dcba1b7a6c508d7169b405c6d7d0ea244
Gaetano Giacalone <dagman62@gmail.com>
2018-08-10 00:58:41 -0700

    added template for testing

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,3 +7,15 @@
 log "Your Platform is, #{node["distro"]}!" do
   level :info
 end
+
+platform = node['platform']
+
+if platform == 'centos' || platform == 'fedora'
+  template '/etc/yum.repos.d/nginx.repo' do
+    source 'nginx.repo.erb'
+      variables ({
+        :distro => node['distro']
+      })
+      action :create
+    end
+end
